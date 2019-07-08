b05d119df32715c3316934e2d8e77faeaad3b21f
Kiana Donaldson <keeyanakellydonaldson@Keeyanas-MacBook-Pro.local>
2019-07-08 16:21:56 -0700

    Begin seed functionality and set up basic server

---
+++ server/database/seed.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+const db = require('./schema.sql');
+
+// Option 1: Passing parameters separately
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+  
